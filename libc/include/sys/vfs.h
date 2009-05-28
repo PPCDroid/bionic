1d1433333dd89c9820da9201a580c3e05c1e528c
Dan Malek <dan@embeddedalley.com>
2009-05-28 11:07:05 -0400

    Need to make the statfs structure match the one the kernel is using.

--- libc/include/sys/vfs.h
+++ libc/include/sys/vfs.h
@@ -36,6 +36,20 @@ __BEGIN_DECLS
 
 /* note: this corresponds to the kernel's statfs64 type */
 struct statfs {
+#ifdef __mips__
+    uint32_t        f_type;
+    uint32_t        f_bsize;
+    uint32_t        f_frsize;	/* Fragment size - unsupported */
+    uint32_t        __pad;
+    uint64_t        f_blocks;
+    uint64_t        f_bfree;
+    uint64_t        f_files;
+    uint64_t        f_ffree;
+    uint64_t        f_bavail;
+    __kernel_fsid_t f_fsid;
+    uint32_t        f_namelen;
+    uint32_t        f_spare[6];
+#else
     uint32_t        f_type;
     uint32_t        f_bsize;
     uint64_t        f_blocks;
@@ -47,6 +61,7 @@ struct statfs {
     uint32_t        f_namelen;
     uint32_t        f_frsize;
     uint32_t        f_spare[5];
+#endif /* __mips__ */
 };
 
 #define  ADFS_SUPER_MAGIC      0xadf5
