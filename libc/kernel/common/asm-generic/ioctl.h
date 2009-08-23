323258b957950765019060afe10c6f7707d21469
Tom Rini <trini@embeddedalley.com>
2009-08-23 13:38:14 -0700

    Allow for architectures to override: _IOC_SIZEBITS, _IOC_DIRBITS, _IOC_NONE, _IOC_WRITE and _IOC_READ.  Fixes some warnings on MIPS and PPC

--- libc/kernel/common/asm-generic/ioctl.h
+++ libc/kernel/common/asm-generic/ioctl.h
@@ -14,8 +14,13 @@
 
 #define _IOC_NRBITS 8
 #define _IOC_TYPEBITS 8
-#define _IOC_SIZEBITS 14
-#define _IOC_DIRBITS 2
+#ifndef _IOC_SIZEBITS
+# define _IOC_SIZEBITS 14
+#endif
+
+#ifndef _IOC_DIRBITS
+# define _IOC_DIRBITS 2
+#endif
 
 #define _IOC_NRMASK ((1 << _IOC_NRBITS)-1)
 #define _IOC_TYPEMASK ((1 << _IOC_TYPEBITS)-1)
@@ -27,9 +32,17 @@
 #define _IOC_SIZESHIFT (_IOC_TYPESHIFT+_IOC_TYPEBITS)
 #define _IOC_DIRSHIFT (_IOC_SIZESHIFT+_IOC_SIZEBITS)
 
-#define _IOC_NONE 0U
-#define _IOC_WRITE 1U
-#define _IOC_READ 2U
+#ifndef _IOC_NONE
+# define _IOC_NONE 0U
+#endif
+
+#ifndef _IOC_WRITE
+# define _IOC_WRITE 1U
+#endif
+
+#ifndef _IOC_READ
+# define _IOC_READ 2U
+#endif
 
 #define _IOC(dir,type,nr,size)   (((dir) << _IOC_DIRSHIFT) |   ((type) << _IOC_TYPESHIFT) |   ((nr) << _IOC_NRSHIFT) |   ((size) << _IOC_SIZESHIFT))
 
