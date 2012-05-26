b1159cdb5ebffdbc31962599bea56637d0b7803b
Dmitry Eremin-Solenikov <dmitry_eremin@mentor.com>
2012-05-26 11:57:43 +0400

    bionic: dladdr first argument should be a constant
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry_eremin@mentor.com>

--- linker/linker.c
+++ linker/linker.c
@@ -559,7 +559,7 @@ Elf32_Sym *lookup(const char *name, soinfo **found, soinfo *start)
     return NULL;
 }
 
-soinfo *find_containing_library(void *addr)
+soinfo *find_containing_library(const void *addr)
 {
     soinfo *si;
 
@@ -573,7 +573,7 @@ soinfo *find_containing_library(void *addr)
     return NULL;
 }
 
-Elf32_Sym *find_containing_symbol(void *addr, soinfo *si)
+Elf32_Sym *find_containing_symbol(const void *addr, soinfo *si)
 {
     unsigned int i;
     unsigned soaddr = (unsigned)addr - si->base;
